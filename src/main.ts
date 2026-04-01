165ad8ba6fa8ffa31502060de0379f704840d47f
SusannaCarlsson90 <susanna.carlsson90@icloud.com>
2026-04-01 17:08:00 +0200

    For each loop som skriver ut på hemsidan

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { StorageUtility } from './utils/storage';
 
+//Skapar en array som bara får innehålla objekt som ser ut som mitt courseInfo interface
+let courseList: courseInfo[] = [];
+
 //Interface för att säkerhetsställa att alla värden skrivs in korrekt. 
 interface courseInfo {
   code: string;
@@ -15,6 +18,7 @@ const courseCode = document.getElementById('courseCode') as HTMLInputElement;
 const courseName = document.getElementById('courseName') as HTMLInputElement;
 const progression = document.getElementById('progression') as HTMLSelectElement;
 const syllabus = document.getElementById('syllabus') as HTMLInputElement;
+const courseTable = document.getElementById('courseTable') as HTMLDivElement;
 
 courseForm.addEventListener('submit', (e) => {
   e.preventDefault(); //Så sidan inte laddas om hela tiden
@@ -23,10 +27,27 @@ courseForm.addEventListener('submit', (e) => {
 const newCourse: courseInfo = {
   code: courseCode.value,
   name: courseName.value,
-  progression: progression.value as 'A' | 'B'
+  progression: progression.value as 'A' | 'B' | 'C',
+  syllabus: syllabus.value,
 };
 
-console.log("Ny kurs", newCourse)
+//"Puttar in värdet användaren skrivit ner in i min array courseList"
+courseList.push(newCourse);
+console.log(courseList);
 
+//Loopar igenom array och skapar HTML för varje enskild kurs
+
+courseTable.innerHTML = ""; //Töm listan
+
+courseList.forEach((course) => { 
+ courseTable.innerHTML += `
+<div> 
+      <p><strong>Kurskod:</strong> ${course.code}</p>
+      <p><strong>Namn:</strong> ${course.name}</p>
+      <p><strong>Progression:</strong> ${course.progression}</p>
+      <p><strong>Syllabus:</strong> ${course.syllabus}</p>
+    </div>
+  `;
+});
 });
 
