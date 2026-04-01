ce9ee5ef5a98a5e81cbc8eb699c8e84f006e0225
SusannaCarlsson90 <susanna.carlsson90@icloud.com>
2026-04-01 14:42:24 +0200

    formulär klart i html

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { StorageUtility } from './utils/storage';
 
 //Interface för att säkerhetsställa att alla värden skrivs in korrekt. 
 interface courseInfo {
-  code: number;
+  code: string;
   name: string;
   progression: 'A' | 'B' | 'C';
   syllabus: string;
