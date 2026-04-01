eb2bc9c145787f9a14f5188b388914ca1d84fb74
SusannaCarlsson90 <susanna.carlsson90@icloud.com>
2026-04-01 16:32:33 +0200

    DOM

--- src/main.ts
+++ src/main.ts
@@ -7,3 +7,26 @@ interface courseInfo {
   progression: 'A' | 'B' | 'C';
   syllabus: string;
 }
+
+//Hämtar mina html element
+
+const courseForm = document.getElementById('courseForm') as HTMLFormElement;
+const courseCode = document.getElementById('courseCode') as HTMLInputElement;
+const courseName = document.getElementById('courseName') as HTMLInputElement;
+const progression = document.getElementById('progression') as HTMLSelectElement;
+const syllabus = document.getElementById('syllabus') as HTMLInputElement;
+
+courseForm.addEventListener('submit', (e) => {
+  e.preventDefault(); //Så sidan inte laddas om hela tiden
+
+//skapar objekt för kursstrukturen utifrån mitt interface 
+const newCourse: courseInfo = {
+  code: courseCode.value,
+  name: courseName.value,
+  progression: progression.value as 'A' | 'B'
+};
+
+console.log("Ny kurs", newCourse)
+
+});
+
